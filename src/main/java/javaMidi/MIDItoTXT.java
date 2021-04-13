73e3c182b8ee0740dae726ea562256afad3fa83c
lordgkram <urumpel@gmx.net>
2021-04-13 15:05:25 +0200

    erste schritte von v0.6

---
+++ src/main/java/javaMidi/MIDItoTXT.java
@@ -0,0 +1,154 @@
+package javaMidi;
+
+import java.io.File;
+
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Track;
+
+public class MIDItoTXT {
+
+    public static String[] convert(File input, int bpm) {
+        try {
+            Sequence seq = MidiSystem.getSequence(input);
+            Track[] tracks = seq.getTracks();
+            System.out.println(tracks.length + " tracks");
+            int ppq = seq.getResolution();
+            String[] out = new String[tracks.length];
+            for (int i = 0; i < tracks.length; i++) {
+                out[i] = trackToString(tracks[i], bpm, ppq);
+                System.out.println(out[i]);
+            }
+            return out;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new String[0];
+        }
+    }
+
+    public static String trackToString(Track t, int bpm, int ppq) {
+        String out = "";
+        int lastBpm = bpm;
+        int ln = 2000;
+        long lt = 0;
+        for (int i = 0; i < t.size(); i++) {
+            MidiEvent ev = t.get(i);
+            MidiMessage msg = ev.getMessage();
+            long tick = ev.getTick();
+            long tlDiff = (long) ((tick - lt) * (60000f / (lastBpm * ppq)));
+            int sysPause = (int) ((240000f / ((float) lastBpm)) / tlDiff);
+            if (sysPause < 128)
+                out += "" + sysPause + " ";
+            System.out.println(sysPause);
+            byte[] data = msg.getMessage();
+            // TODO: MULTI CHANAL!!!
+            if (data.length == 3) {
+                int datan = (data[0] & 0xF0) >> 4;
+                if (datan == 0b1000 || datan == 0b1001) {
+                    // note on off
+                    if (data[1] == ln) {
+                        out += "l";
+                    } else {
+                        out += note2str(data[1]);
+                    }
+                    ln = data[1];
+                } else if (datan == 0b1011) {
+                    // controll change
+                    int controll = (data[0] & 0x7F);
+                    if (controll == 123) {
+                        // alloff
+                        out += "s";
+                    } else if (controll == 7) {
+                        // volume
+                        out += "v" + data[1] + "";
+                    } else {
+                        System.out.println("Unknown controll:" + controll);
+                    }
+                } else {
+                    System.out.println("Unknown command:" + datan + " = " + data[1] + ", " + data[2]);
+                }
+            } else if (data.length == 2) {
+                int datan = (data[0] & 0xF0) >> 4;
+                if (datan == 0b1100) {
+                    // prg change
+                    out += "i" + data[1] + "";
+                } else {
+                    System.out.println("2byte Unknown command:" + datan);
+                }
+            } else {
+                System.out.println(data.length + " paket unknown! 1. byte: " + data[0]);
+            }
+            lt = tick;
+        }
+        return "-bpm" + bpm + " " + out;
+    }
+
+    public static String note2str(int n) {
+        String e = "";
+        String s = "";
+        switch (n % 12) {
+        case 0:
+            e = "c";
+            s = "";
+            break;
+        case 1:
+            e = "c";
+            s = "#";
+            break;
+        case 2:
+            e = "d";
+            s = "";
+            break;
+        case 3:
+            e = "d";
+            s = "#";
+            break;
+        case 4:
+            e = "e";
+            s = "";
+            break;
+        case 5:
+            e = "f";
+            s = "";
+            break;
+        case 6:
+            e = "f";
+            s = "#";
+            break;
+        case 7:
+            e = "g";
+            s = "";
+            break;
+        case 8:
+            e = "g";
+            s = "#";
+            break;
+        case 9:
+            e = "a";
+            s = "";
+            break;
+        case 10:
+            e = "a";
+            s = "#";
+            break;
+        case 11:
+            e = "h";
+            s = "";
+            break;
+        }
+        String str = "";
+        int oktavep = (n - (n % 12)) / 12;
+        if (oktavep <= 4) {
+            for (int i = 0; i < 4 - oktavep; i++)
+                str += "'";
+            e = e.toUpperCase();
+        } else {
+            for (int i = 0; i < oktavep - 5; i++)
+                str += "'";
+        }
+        return e + str + s;
+    }
+
+}
--- src/main/java/javaMidi/Window.java
+++ src/main/java/javaMidi/Window.java
@@ -4,16 +4,26 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.datatransfer.StringSelection;
+import java.awt.Toolkit;
+import java.io.File;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JSlider;
 import javax.swing.JSpinner;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
@@ -61,14 +71,14 @@ public class Window {
 		settings.add(new JLabel("Puffer prioritaet:"));
 		settings.add(new JLabel("Lautstaerke:"));
 		settings.add(doBuffer);
-		
+
 		settings.add(name);
 		settings.add(maxPlayTime);
 		settings.add(maxBuffer);
 		settings.add(prio);
 		settings.add(volumen);
 		settings.add(send);
-		
+
 		volumen.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent arg0) {
@@ -79,9 +89,13 @@ public class Window {
 		send.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
+				if (!send.isEnabled())
+					return;
 				send.setEnabled(false);
-				String head = "{\"aktiviereBuffer\":" + (doBuffer.isSelected() ? "true" : "false") + ",\"laenge\":" + String.valueOf(maxPlayTime.getValue()) + "," + "\"maximaleBufferGroesse\":"
-						+ String.valueOf(maxBuffer.getValue()) + ",\"prioritaet\":" + String.valueOf(prio.getValue()) + ",\"nutzer\":\"" + name.getText() + "\",\"midi\":\"";
+				String head = "{\"aktiviereBuffer\":" + (doBuffer.isSelected() ? "true" : "false") + ",\"laenge\":"
+						+ String.valueOf(maxPlayTime.getValue()) + "," + "\"maximaleBufferGroesse\":"
+						+ String.valueOf(maxBuffer.getValue()) + ",\"prioritaet\":" + String.valueOf(prio.getValue())
+						+ ",\"nutzer\":\"" + name.getText() + "\",\"midi\":\"";
 				String suffix = "\"}";
 				String[] toPlay = input.getText().split("\n");
 				progress.setMaximum(toPlay.length);
@@ -118,7 +132,167 @@ public class Window {
 
 		jf.setContentPane(content);
 
+		JMenuBar jmb = new JMenuBar();
+
+		JMenu jmTools = new JMenu("Tools");
+
+		JMenuItem jmiMidiFileConverter = new JMenuItem(".mid converter");
+		jmiMidiFileConverter.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JFrame imp = getMidiImport();
+				if (imp == null)
+					return;
+				imp.setSize(1280, 720);
+				imp.setVisible(true);
+			}
+		});
+		jmTools.add(jmiMidiFileConverter);
+
+		JMenuItem jmiTWFormetter = new JMenuItem("Twitch Formetter");
+		jmiTWFormetter.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JFrame imp = getTwitchFormatter();
+				if (imp == null)
+					return;
+				imp.setSize(1280, 720);
+				imp.setVisible(true);
+			}
+		});
+		jmTools.add(jmiTWFormetter);
+
+		jmb.add(jmTools);
+
+		jf.setJMenuBar(jmb);
+
 		jf.setVisible(true);
+	}
+
+	public JFrame getMidiImport() {
+		JFrame e = new JFrame(
+				"ProjektionTV Midi Emulator v" + Main.MAJOR_VERSION + "." + Main.MINOR_VERSION + " .mid Convertierer");
+		e.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		JFileChooser jfc = new JFileChooser();
+		int rtn = jfc.showOpenDialog(e);
+
+		if (rtn == JFileChooser.APPROVE_OPTION) {
+			File f = jfc.getSelectedFile();
+			e.setTitle(e.getTitle() + " : " + f.getName());
+			String bpmstr = JOptionPane.showInputDialog(e, "Import bpm", "120");
+			int bpm = Integer.parseInt(bpmstr);
+			String[] data = MIDItoTXT.convert(f, bpm);
+
+			JTabbedPane jtb = new JTabbedPane();
+			e.setContentPane(jtb);
+
+			for (int i = 0; i < data.length; i++) {
+				JTextArea jta = new JTextArea(data[i]);
+				jta.setEditable(false);
+				jta.setLineWrap(true);
+
+				JScrollPane jsp = new JScrollPane(jta);
+
+				JPanel order = new JPanel(new BorderLayout());
+				order.add(jsp, BorderLayout.CENTER);
+
+				JButton jb = new JButton("Copy to Clipbord");
+				jb.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(jta.getText()),
+								null);
+					}
+				});
+				order.add(jb, BorderLayout.SOUTH);
+
+				jtb.addTab("Track " + (i + 1), order);
+			}
+		} else {
+			e = null;
+		}
+
+		return e;
+	}
+
+	public JFrame getTwitchFormatter() {
+		JFrame e = new JFrame(
+				"ProjektionTV Midi Emulator v" + Main.MAJOR_VERSION + "." + Main.MINOR_VERSION + " Twitch Formatter");
+		e.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		JPanel content = new JPanel(new BorderLayout());
+
+		JTextArea jta = new JTextArea(input.getText(), 1, 500);
+		jta.setEditable(true);
+		JScrollPane jsc = new JScrollPane(jta);
+		content.add(jsc, BorderLayout.NORTH);
+
+		JTabbedPane jtp = new JTabbedPane();
+		jtp.addTab("", new JLabel("Bitte Formatieren drücken."));
+		content.add(jtp, BorderLayout.CENTER);
+
+		JButton jb = new JButton("Formatieren");
+		jb.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String in = jta.getText();
+				jtp.removeAll();
+				if (in.length() <= 500) {
+					JPanel t = new JPanel(new BorderLayout());
+					JTextArea jtaT = new JTextArea(in);
+					jtaT.setEditable(false);
+					JScrollPane jspT = new JScrollPane(jtaT);
+					t.add(jspT, BorderLayout.CENTER);
+					JButton jb = new JButton("Copy to Clipbord");
+					jb.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent arg0) {
+							Toolkit.getDefaultToolkit().getSystemClipboard()
+									.setContents(new StringSelection(jtaT.getText()), null);
+						}
+					});
+					t.add(jb, BorderLayout.NORTH);
+					jtp.addTab("1", t);
+				} else {
+					String[] e = in.split(" ");
+					ArrayList<String> datas = new ArrayList<>();
+					String local = ";ln";
+					for (int i = 0; i < e.length; i++) {
+						if ((local + " " + e[i]).length() <= 500) {
+							local += " " + e[i];
+						} else {
+							datas.add(local);
+							local = new String();
+							local = "; " + e[i];
+						}
+					}
+					datas.add(local.substring(2));
+					for (int i = 0; i < datas.size(); i++) {
+						JPanel t = new JPanel(new BorderLayout());
+						JTextArea jtaT = new JTextArea(datas.get(i));
+						jtaT.setEditable(false);
+						JScrollPane jspT = new JScrollPane(jtaT);
+						t.add(jspT, BorderLayout.CENTER);
+						JButton jb = new JButton("Copy to Clipbord");
+						jb.addActionListener(new ActionListener() {
+							@Override
+							public void actionPerformed(ActionEvent arg0) {
+								Toolkit.getDefaultToolkit().getSystemClipboard()
+										.setContents(new StringSelection(jtaT.getText()), null);
+							}
+						});
+						t.add(jb, BorderLayout.NORTH);
+						jtp.addTab((i + 1) + "", t);
+					}
+				}
+			}
+		});
+		content.add(jb, BorderLayout.SOUTH);
+
+		e.setContentPane(content);
+
+		return e;
 	}
 
 }
